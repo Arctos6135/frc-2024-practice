f0ec029796630030c0263ea31df15ed22d9f06a5
n-arms <75282394+n-arms@users.noreply.github.com>
2024-02-28 12:10:00 -0500

    Update pracctice dt from talonFx's to talonSRX's

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,16 +4,23 @@ import frc.robot.constants.CANBus;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
 
 
 public class Drivetrain extends SubsystemBase {
-    private final TalonFX rightMaster = new TalonFX(CANBus.RIGHT_MASTER);
-    private final TalonFX leftMaster = new TalonFX(CANBus.LEFT_MASTER);
-    private final TalonFX rightFollower = new TalonFX(CANBus.RIGHT_FOLLOWER);
-    private final TalonFX leftFollower = new TalonFX(CANBus.LEFT_FOLLOWER);
+    // private final TalonFX rightMaster = new TalonFX(CANBus.RIGHT_MASTER);
+    // private final TalonFX leftMaster = new TalonFX(CANBus.LEFT_MASTER);
+    // private final TalonFX rightFollower = new TalonFX(CANBus.RIGHT_FOLLOWER);
+    // private final TalonFX leftFollower = new TalonFX(CANBus.LEFT_FOLLOWER);
+
+    private final TalonSRX rightMaster = new TalonSRX(CANBus.RIGHT_MASTER);
+    private final TalonSRX leftMaster = new TalonSRX(CANBus.LEFT_MASTER);
+    private final TalonSRX rightFollower = new TalonSRX(CANBus.RIGHT_FOLLOWER);
+    private final TalonSRX leftFollower = new TalonSRX(CANBus.LEFT_FOLLOWER);
 
     private final SlewRateLimiter translationLimiter = new SlewRateLimiter(3);
     private final SlewRateLimiter rotationLimiter =  new SlewRateLimiter(2);
@@ -39,10 +46,10 @@ public class Drivetrain extends SubsystemBase {
     }
 
     private void setMotors(double left, double right) {
-        leftMaster.set(left);
-        leftFollower.set(left);
-        rightMaster.set(right);
-        rightFollower.set(right);
+        leftMaster.set(TalonSRXControlMode.PercentOutput, left);
+        leftFollower.set(TalonSRXControlMode.PercentOutput, left);
+        rightMaster.set(TalonSRXControlMode.PercentOutput, right);
+        rightFollower.set(TalonSRXControlMode.PercentOutput, right);
     }
 
     public void arcadeDrive(double translation, double rotation) {
